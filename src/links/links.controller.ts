d2e622a997c7d7e2eadcb42bc163feaf9a02290a
root <root@tihon-main.ml>
2019-11-14 19:44:13 +0300

    fix: remove unused Put variable

--- src/links/links.controller.ts
+++ src/links/links.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Param, Body, Put, Delete } from '@nestjs/common'
+import { Controller, Get, Post, Param, Body, Delete } from '@nestjs/common'
 import { CreateLinkDto } from './dto/create-link.dto'
 import { LinksService } from './links.service'
 import { Link } from './interfaces/link.interface'
